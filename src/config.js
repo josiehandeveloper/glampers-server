97c1a2903b3fc78eeac832285b9d74d8579cd840
Josephine Han <jsphnhan@gmail.com>
2021-06-17 00:53:14 -0700

    took off / in client origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "https://glampers.vercel.app/",
+  CLIENT_ORIGIN: "https://glampers.vercel.app",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://josephinehan@localhost/glampers",
   JWT_SECRET: process.env.JWT_SECRET || "glampers-jwt",
